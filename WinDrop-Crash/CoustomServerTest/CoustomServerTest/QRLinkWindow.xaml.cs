979e68fb9fb53ce6c5d3a4a6cf23eae338d3929d
Pedro López <thepekingst@gmail.com>
2016-02-18 15:42:40 +0100

    Project reborn

---
+++ WinDrop-Crash/CoustomServerTest/CoustomServerTest/QRLinkWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace CoustomServerTest
+{
+    /// <summary>
+    /// Lógica de interacción para QRLinkWindow.xaml
+    /// </summary>
+    public partial class QRLinkWindow : Window
+    {
+        public QRLinkWindow(PedroFileDownload fileData)
+        {
+
+            InitializeComponent();
+            //PayloadGenerator.Url url2 = new PayloadGenerator.Url("www.google.es");
+            PayloadGenerator.Url url = new PayloadGenerator.Url(fileData.Url + @"/index.html");
+            //PayloadGenerator.WiFi wifiPayload = new PayloadGenerator.WiFi("MyWiFi-SSID", "MyWiFi-Pass", PayloadGenerator.WiFi.Authentication.WPA);
+            QRCodeGenerator.ECCLevel  eccLevel= QRCodeGenerator.ECCLevel.L;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            //QRCodeData qrCodeData = qrGenerator.CreateQrCode(@fileData.Url+@"\index.html", eccLevel);
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url.ToString(), eccLevel);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeBitmap = qrCode.GetGraphic(20, "#000000", "#ffffff");
+
+            ImageSource qrImageSource =ConvertBitmapToBitmapImage(qrCodeBitmap);
+            imgQrCode.Source = qrImageSource;
+        }
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bmp)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bmp.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ WinDrop/CoustomServerTest/PedroServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoustomServerTest
+{
+    // http://rlbisbe.net/2011/04/25/creando-un-servidor-http-basico-con-c/
+    class PedroServer
+    {
+        Int32 port;
+        IPAddress localAddr;
+        TcpListener server = null;
+        Byte[] bytes;
+        String data;
+
+        public PedroServer()
+        {
+            this.port = 4564; // puerto
+            this.localAddr = IPAddress.Parse("127.0.0.1"); // para escuchar localhost, o sea, nuestros paquetes
+            this.server = new TcpListener(localAddr, port); //listener con el puerto y la IP
+            server.Start(); // iniciamos el servidor
+            this.bytes = new Byte[1000];
+            this.data = null; // datos procesados
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient(); //Aceptamos la conexión entrante
+
+            }
+        }
+
+
+
+        private void sendBasicPacket()
+        {
+
+        }
+
+    }
+}
